b9f6f82f6cf7456483c822355cf378a3c169a552
xuj266 <xuj266@vanke.com>
2024-09-26 11:42:38 +0800

    init

---
+++ src/main/java/com/endlessovo/assistantGPT/AssistantGptApplication.java
@@ -0,0 +1,13 @@
+package com.endlessovo.assistantGPT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssistantGptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssistantGptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/endlessovo/assistantGPT/AssistantGptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.endlessovo.assistantGPT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssistantGptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
