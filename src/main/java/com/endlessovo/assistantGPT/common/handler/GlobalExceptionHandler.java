c8a0d36607b812f0f535d77dba3ba8bfbf3c3f04
xuj266 <xuj266@vanke.com>
2024-09-30 09:41:38 +0800

    feat: 修正全局异常捕获

--- src/main/java/com/endlessovo/assistantGPT/common/handler/GlobalExceptionHandler.java
+++ src/main/java/com/endlessovo/assistantGPT/common/handler/GlobalExceptionHandler.java
@@ -6,10 +6,11 @@ import com.endlessovo.assistantGPT.common.exception.CustomExceptionEnum;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @Slf4j
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(CustomException.class)
